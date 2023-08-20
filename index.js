e8bb34a6d6743ed4f3302dfcca18f61251f214c4
c0nN37 <15908258-c0nN37@users.noreply.replit.com>
2023-08-20 01:03:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let myHttp = require('http');
+
+let myServer = myHttp.createServer(
+  function(myRequest, myResponse) {
+    console.log(myRequest.url);
+
+    let myText;
+    if (myRequest.url === '/treasure'){
+      myText = "<span style='font-size:100px;'>&#128142;</span>";
+    } else {
+      myText = "<h1>Can you find the treasure?</h1>"
+    }
+    
+    myResponse.writeHead(200, { "Content-Type": "text/html"} );
+
+    myResponse.end(myText);
+  }
+);
+
+myServer.listen(8080, "0.0.0.0");
+
+console.log("Hint: The password is /treasure");
